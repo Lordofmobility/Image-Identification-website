014f3754f0359daa81d82340a2c9f84fb80677e0
CowiVM <72988033+CowiVM@users.noreply.github.com>
2021-01-15 20:40:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:"jpeg",
+    jpeg_quality:90
+})
+
+camera = document.getElementById("camera");
+
+Webcam.attach(camera)
+
+function take_snapshot() {
+Webcam.snap(function(data_uri) {
+    document.getElementById("result").innerHTML = "<img id='capture_image' src ='" + data_uri +"'/>"
+})
+}
+
+console.log("ml5 version = " + ml5.version);
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/O1cyoACUi/model.json", modelLoaded)
+
+function modelLoaded() {
+    console.log("Model Loaded");
+}
+
+function check() {
+    img = document.getElementById("capture_image");
+    classifier.classify(img, got_result)
+}
+
+function got_result(error, results) {
+    if(error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
